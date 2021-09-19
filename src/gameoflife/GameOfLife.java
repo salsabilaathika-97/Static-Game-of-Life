ed965acd372416bd0295468bbbb74499bc05f6c1
salsabilaathika-97 <salsabilaathika@student.ub.ac.id>
2021-09-19 14:25:24 +0800

    add 2 neighbours between 1 person alive

--- src/gameoflife/GameOfLife.java
+++ src/gameoflife/GameOfLife.java
@@ -21,6 +21,20 @@ public class GameOfLife {
                 }
                 System.out.println("");
             }
+            System.out.println("");
+        //add 2 neighbors between 1 person alive
+            for (int i=0;i<10;i++){
+                for(int j=0;j<10;j++){
+                    Grid[i][j] = 0;
+                    Grid[6][4] = 1;
+                    Grid[6][3] = 1;
+                    Grid[6][5] = 1;
+                    System.out.print(Grid[i][j]+" ");
+                }
+                System.out.println("");
+            }
+            System.out.println("");
+        
             
             
     }   
