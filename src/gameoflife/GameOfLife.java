4f24489095c5711846f52cbb8bbce8c4e2e84e53
salsabilaathika-97 <salsabilaathika@student.ub.ac.id>
2021-09-19 15:30:52 +0800

    add 2nd example to main class

--- src/gameoflife/GameOfLife.java
+++ src/gameoflife/GameOfLife.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 public class GameOfLife extends Example {
     public static void main(String[] args) {
         //create example class to store models
-//        Scanner scan = new Scanner(System.in);
-//        System.out.println("Pick example");
-//        System.out.println("1. Oscillator");
-//        System.out.print("Your answer: ");
-//        int answer = scan.nextInt();
-//        if (answer == 1) {
-//           Example e1 = new Example();
-//           e1.Oscillator();
-//        }
-    Example e1 = new Example();
-    e1.ReverseOscillator();
+        Scanner scan = new Scanner(System.in);
+        Example ex = new Example();
+        System.out.println("Pick example");
+        System.out.println("1. Oscillator");
+        System.out.println("2. Reverse Oscillator");
+        System.out.print("Your answer: ");
+        int answer = scan.nextInt();
+        if (answer == 1) {
+           ex.Oscillator();
+        } else if (answer == 2) {
+           ex.ReverseOscillator();
+        }
     }   
 }
