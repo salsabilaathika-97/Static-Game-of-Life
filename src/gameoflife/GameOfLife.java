4147bbd9796ff74eb1e3c5fe6b63e6f0f48865bc
salsabilaathika-97 <salsabilaathika@student.ub.ac.id>
2021-09-19 15:05:53 +0800

    add scanner input step 1

--- src/gameoflife/GameOfLife.java
+++ src/gameoflife/GameOfLife.java
@@ -1,8 +1,16 @@
 package gameoflife;
+import java.util.Scanner;
 public class GameOfLife extends Example {
     public static void main(String[] args) {
         //create example class to store models
-        Example e1 = new Example();
-        e1.Oscillator();
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Pick example");
+        System.out.println("1. Oscillator");
+        int answer = scan.nextInt();
+        if (answer == 1) {
+           Example e1 = new Example();
+           e1.Oscillator();
+        }
+       
     }   
 }
