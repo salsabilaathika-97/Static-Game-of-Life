4d6a059306f01c16692bf85e47a920f32fe604a8
salsabilaathika-97 <salsabilaathika@student.ub.ac.id>
2021-09-19 14:16:36 +0800

    add 1 person alive

--- src/gameoflife/GameOfLife.java
+++ src/gameoflife/GameOfLife.java
@@ -3,16 +3,25 @@ package gameoflife;
 public class GameOfLife {
     public static void main(String[] args) {
         int [][] Grid = new int [10][10];
+        //initial cell
             for (int i=0;i<10;i++){
                 for(int j=0;j<10;j++){
                     Grid[i][j] = 0;
                     System.out.print(Grid[i][j]+" ");
-                   
+                }
+                System.out.println("");
+            }
+            System.out.println("");
+        //add 1 person alive
+            for (int i=0;i<10;i++){
+                for(int j=0;j<10;j++){
+                    Grid[i][j] = 0;
+                    Grid[6][4] = 1;
+                    System.out.print(Grid[i][j]+" ");
                 }
                 System.out.println("");
             }
             
             
-    }
-    
+    }   
 }
